66ef837f4e0722c9fe55433810de3b84e8c62574
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-10-13 13:19:59 -0400

    xx

---
+++ app/HomePage/InfoPage.jsx
@@ -0,0 +1,36 @@
+'use client'
+import { Button, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@nextui-org/react'
+import React from 'react'
+
+function InfoPage({ showMenu, setShowMenu }) {
+
+
+    const menuName = showMenu.currentMenu
+    return (
+        < Modal isOpen={showMenu} backdrop={'transprent'} onOpenChange={() => { setShowMenu(false) }
+        } placement='auto' size='full' scrollBehavior='inside' className={`h-auto  text-white w-full overflow-x-hidden bg-[#ffcfa7]   ${{
+            backdrop: "bg-black bg-opacity-100"
+        }}`}>
+            <ModalContent>
+                {() => (
+                    <>
+                        <ModalHeader className="center-col gap-1   md:scroll-px-20 ">
+                            {menuName}
+                        </ModalHeader>
+                        <ModalBody className='hidescroll  overflow-hidden    p-2'>
+
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button className='w-full' onPress={() => { setShowMenu(false) }} color="danger" variant="light">
+                                Close
+                            </Button>
+
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal >
+    )
+}
+
+export default InfoPage
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+'use client'
 import { Button, Image } from '@nextui-org/react';
+import InfoPage from '@/app/HomePage/InfoPage'
+import { useState } from 'react';
+
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <div className="bg-white  min-h-screen w-screen  pb-20  sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-white  min-h-screen w-screen  pb-10  sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col">
         <div className='relative w-full h-[36.5rem] overflow-hidden'>
           <div className="absolute h-full bg-black rounded-b-xl bg-opacity-25 w-full z-20 center-col text-white">
@@ -10,6 +16,7 @@ export default function Home() {
           </div>
           <Image className='w-full h-full rounded-t-none object-cover' src='https://images.unsplash.com/photo-1570545917537-873e36d4f64a?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
         </div>
+        <InfoPage showMenu={showMenu} setShowMenu={setShowMenu} />
         <div className='grid grid-cols-3 w-96 gap-2 p-2 items-center justify-center'>
           {['How to enroll', 'Health & Safety', 'More than a babysitter',].map((item, index) => {
             const imgs = [
@@ -18,7 +25,7 @@ export default function Home() {
               'https://www.kindercare.com/-/media/kindercare/features/personalization/kclc_homepage_personalization_tile_morethandaycare.jpg?h=800&w=800&la=en&hash=81D83D3016EBF6230C5A0DA72FD371FD'
             ]
             return (
-              <Button draggable={true} className='h-32 p-0 md:h-32  col-span-1 text-center' key={item}>
+              <Button onPress={() => { setShowMenu({ currentMenu: item }) }} className='h-32 p-0 md:h-32  col-span-1 text-center' key={item}>
                 <h1 className='text-center w-fit text-wrap absolute z-20 bottom-2 text-white font-bold'>{item}</h1>
                 <Image className='h-32 w-full' src={imgs[index]} alt='' />
               </Button>
