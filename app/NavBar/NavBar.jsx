e332892cd0c76c85e597f56a655c0f68d913c327
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-11-06 11:08:44 -0500

    x

--- app/NavBar/NavBar.jsx
+++ app/NavBar/NavBar.jsx
@@ -2,11 +2,10 @@
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Image, Link, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { AiOutlineDotChart } from 'react-icons/ai';
 import { FaBabyCarriage } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
 import { LuMenuSquare } from "react-icons/lu";
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import { GoDotFill } from "react-icons/go";
 function NavBar() {
     const [showMenu, setShowMenu] = useState(false)
     const menuItem = ['About Us', 'Contact Us', 'Find My Nanny', 'Find My Family']
